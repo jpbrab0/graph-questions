593e86d36f2a783b1c2f17aa667dd7361f7ca017
João Pedro Resende <jpresende502@gmail.com>
2022-04-23 23:54:26 -0300

    feat: Creating Question repository and adding clean code in question service

---
+++ src/entities/question/resolvers.ts
@@ -0,0 +1,22 @@
+import { context } from '../../context';
+import { QuestionRepository } from '../../repositories/QuestionRepository';
+import { QuestionService } from '../../services/QuestionService';
+
+const questionRepository = new QuestionRepository(context.prisma);
+const service = new QuestionService(questionRepository);
+
+export const questionResolver = {
+	Mutation: {
+		createQuestion: (_, args, __) => {
+			const { data } = args;
+
+			return service.create(data);
+		},
+		readQuestion: (_, { id }, __) => {
+			return service.read(id);
+		},
+		deleteQuestion: (_, { id }, __) => {
+			return service.delete(id);
+		},
+	},
+};
